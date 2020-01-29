e367dcdb449bc80e8194125a5343cf145d7f11b1
alexshavlovsky <ctzn@tut.by>
2020-01-29 21:37:03 +0300

    refactor MessageFormatter class

--- src/main/java/telegrambot/MessageFormatter.java
+++ src/main/java/telegrambot/MessageFormatter.java
@@ -1,29 +1,29 @@
 package telegrambot;
 
 import org.joda.time.DateTime;
+import telegrambot.apimodel.Chat;
 import telegrambot.apimodel.Message;
 import telegrambot.apimodel.User;
 import telegrambot.io.BotService;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 class MessageFormatter {
 
     private static int maxNameLength = -1;
 
-    private static int updateAndGetMaxNameLength(String name, BotService botService) {
-        if (maxNameLength == -1)
-            maxNameLength = botService.getUsers().stream()
-                    .map(MessageFormatter::formatName)
-                    .map(String::length)
-                    .max(Integer::compare).orElseGet(() -> 0);
-        if (name.length() > maxNameLength) maxNameLength = name.length();
-        return maxNameLength;
+    private static int updateAndGetMaxNameWidth(String name, BotService botService) {
+        if (maxNameLength == -1) maxNameLength = getMaxNameWidth(botService);
+        return Math.max(maxNameLength, name.length());
     }
 
-    private static void addNotBlank(List<String> list, String entry) {
-        if (entry != null && !entry.isEmpty() && !entry.isBlank()) list.add(entry);
+    private static int getMaxNameWidth(BotService botService) {
+        return maxNameLength = botService.getUsers().stream()
+                .map(MessageFormatter::formatName)
+                .map(String::length)
+                .max(Integer::compare).orElse(0);
     }
 
     private static String appendSpace(String s, int n) {
@@ -32,19 +32,32 @@ class MessageFormatter {
         return builder.toString();
     }
 
+    private static String joinNotNullNotBlank(String... entries) {
+        return Arrays.stream(entries).filter(e -> e != null && !e.isBlank()).collect(Collectors.joining(" "));
+    }
+
     static String formatName(User user) {
-        List<String> entries = new ArrayList<>();
-        addNotBlank(entries, user.getFirst_name());
-        addNotBlank(entries, user.getLast_name());
-        return String.join(" ", entries);
+        return joinNotNullNotBlank(user.getFirst_name(), user.getLast_name(), user.getUsername());
+    }
+
+    private static String formatChat(Chat chat) {
+        return joinNotNullNotBlank(chat.getFirst_name(), chat.getLast_name(), chat.getUsername(), chat.getTitle());
+    }
+
+    private static String formatDirection(Message message, BotService botService) {
+        return botService.getToken().contains(message.getFrom().getId().toString()) ? ">" : "<";
+    }
+
+    private static String formatTime(Date date) {
+        return new DateTime(date).toString("HH:mm:ss");
     }
 
     static String format(Message message, BotService botService) {
         String name = formatName(message.getFrom());
-        name = appendSpace(name, updateAndGetMaxNameLength(name, botService));
-        String dir = botService.getToken().contains(message.getFrom().getId().toString()) ? ">" : "<";
-        String time = new DateTime(message.getDate()).toString("HH:mm:ss");
-        return time + " " + name + " " + dir + " " + message.getText();
+        int nameWidth = updateAndGetMaxNameWidth(name, botService);
+        String dir = formatDirection(message, botService);
+        String time = formatTime(message.getDate());
+        return joinNotNullNotBlank(time, appendSpace(name, nameWidth), dir, message.getText());
     }
 
 }
